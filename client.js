a8d1275aafb4a717e6be9beab4ee26f6dc73a4c5
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-09-22 19:16:25 +0000

    Add name to the snake

--- client.js
+++ client.js
@@ -2,17 +2,23 @@ const net = require('net');
 
 // establishes a connection with the game server
 const connect = function () {
+  const name = 'AC';
+
   const conn = net.createConnection({
     host: '135.23.223.133', // IP address here,
     port: 50542 // PORT number here,
-  }, () => {
-    conn.on('data', (data) => {
-      console.log(data);
-    });
   });
 
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');    
+    conn.write(`Name: ${name}`);
+  })
+  
+  conn.setEncoding('utf8'); // interpret incoming data as text
 
   return conn;
 };
