53b373896d4d466434d44c84d83211a331004b38
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-09-22 17:42:50 +0000

    Add client.js file

--- play.js
+++ play.js
@@ -1,21 +1,6 @@
 const net = require('net');
+const connect = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133', // IP address here,
-    port: 50542 // PORT number here,
-  });
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-
-
-  return conn;
-};
 
 console.log('Connecting ...');
 connect();
