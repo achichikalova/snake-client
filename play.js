f6ae7f4009faa7daa27bf9a1d6cb3ae3fa13ae19
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-09-22 21:25:48 +0000

    Snake WIP annd input.js file, refactored play.js

--- play.js
+++ play.js
@@ -1,19 +1,7 @@
 const net = require('net');
 const connect = require('./client');
+const setupInput = require('./input');
 
-const setupInput = () => { // setup interface to handle user input from stdin
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-const handleUserInput = (key) => { 
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
 console.log('Connecting ...');
 
